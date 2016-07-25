be804ffbbef80e648971d44e818d287aa4afcd21
katkamrachana <katkam.rachana@gmail.com>
2016-07-25 15:00:08 +0530

    Parse tables data from .docx file

---
+++ maths_template_parse.py
@@ -0,0 +1,15 @@
+from docx import Document
+
+document = Document('CLIxmaths_U1_A1.2Digital_v1.0 (1).docx')
+tables = document.tables
+print len(tables)
+for table in tables:
+	rows = table.rows
+	for ind,row in enumerate(rows):
+		cells = row.cells
+		for cell in cells:
+			if "Task" == cell.text: 
+				c = table.row_cells(ind)
+				for e in c:
+					print e.text
+					print "*"*80
