35b5b3039f0769a552ff8da84791a6541c866052
katkamrachana <katkam.rachana@gmail.com>
2016-08-01 17:07:06 +0530

    Images/Videos fetch logic modified

--- eng_authoring.py
+++ eng_authoring.py
@@ -1,15 +1,25 @@
 '''
 Please find updated script here: https://github.com/katkamrachana/clix
+Accept file name as arg
 '''
 
 import xlrd
 import  mimetypes
 import os
 from html import HTML
-import eng_master_doc
+# import eng_master_doc
+# activities_template_file = raw_input('Enter Template path(.xlsx file): ')
+# media_mgmt_file = raw_input('Enter Media Mgmt file path (.xlsx file): ')
+# activities_template_workbook = xlrd.open_workbook(activities_template_file)
+# media_mgmt_workbook = xlrd.open_workbook(activities_template_file)
+activities_template_workbook = xlrd.open_workbook('../U1L01 Final Template.xlsx')
 
-workbook = xlrd.open_workbook('../U1 L1 Activities Template.xlsx')
-print "\n Total sheets: ", workbook.nsheets
+media_mgmt_workbook = xlrd.open_workbook('../English_MAM_U01L01.xlsx')
+print "\n Total Template sheets: ", activities_template_workbook.nsheets
+print "\n Total Media sheets: ", media_mgmt_workbook.nsheets
+if media_mgmt_workbook.nsheets == 0:
+	print "\n More than 1 sheet found in Media Mgmt file.\n\n" 
+	"Stopping execution."
 doc_defination = '<?xml version="1.0" encoding="utf-8" standalone="no"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">'
 
 
@@ -51,14 +61,17 @@ def fill_dict(sheet_obj, row_index):
 	each_sheet_on_screen_data_keys = ['Image', 'Video', 'Text', 'Word cloud', 'Audio Track', 'Subtitles (Audio)', 'Subtitles (Video)']
 	key_list = sheet_obj.col_values(1, row_index, row_index + 7)
 	fval_list = sheet_obj.col_values(2, row_index, row_index + 7)
+	# print "\n key_list", key_list
+	# print "\n fval_list",fval_list
 	each_sheet_on_screen_data = {}
 	try:
-		each_sheet_on_screen_data = dict((k, str(v)) for k, v in zip(key_list, fval_list) if v and unicode(k) in each_sheet_on_screen_data_keys)
+		each_sheet_on_screen_data = dict((k, v) for k, v in zip(key_list, fval_list) if v and v != u'None' and unicode(k).strip() in each_sheet_on_screen_data_keys)
 	except UnicodeEncodeError as e:
 		pass
 	except Exception as fill_dict_err:
 		print "\n Error occurred in fill_dict!!! ", fill_dict_err
 		pass
+	# print"\neach_sheet_on_screen_data: ", each_sheet_on_screen_data
 	return each_sheet_on_screen_data
 
 
@@ -192,31 +205,36 @@ def parseOnScreenEle(dictionary_obj, body_DOM):
 	for ele_type, ele_val in dictionary_obj.items():
 		each_sheet_on_screen_data_keys = ['Image', 'Video', 'Text', 'Word cloud', 'Audio Track', 'Subtitles (Audio)', 'Subtitles (Video)']
 		# print ele_type, " === " ,ele_val
-
-		if ele_type == "Text":
+		if ele_type.strip() == "Text" and ele_val != u'NONE':
 			new_val = ele_val.replace('\n', '<br />')
 			section_ele = body_DOM.section
 			section_ele.p(new_val,escape=False)
-		if ele_type == "Image":
-			sheet_imgs_path = 'images/' + each_sheet.name
-			if os.path.exists(sheet_imgs_path):
-				filelist = os.listdir(sheet_imgs_path)
-				img_files = []
-				for eachfile in filelist:
-					try:
-						valid_img = mimetypes.guess_type(sheet_imgs_path + '/' +eachfile)
-					except Exception as valid_img_err:
-						valid_img = None
-					if valid_img and 'image' in valid_img[0]:
-						img_files.append(eachfile)
-				# print "\n final list: ", img_files
-				if img_files:
-					getImgRowHTML(img_files, body_DOM)
+		if ele_type.strip() == "Image" and ele_val != u'NONE':
+			print "\n ele_val", ele_val
+			print "\n Imagefound "
+			print ele_val
+			img_files = []
+			img_files = ele_val.split(',')
+			print img_files
+			# sheet_imgs_path = 'images/' + each_sheet.name
+			# if os.path.exists(sheet_imgs_path):
+			# 	filelist = os.listdir(sheet_imgs_path)
+			# 	img_files = []
+			# 	for eachfile in filelist:
+			# 		try:
+			# 			valid_img = mimetypes.guess_type(sheet_imgs_path + '/' +eachfile)
+			# 		except Exception as valid_img_err:
+			# 			valid_img = None
+			# 		if valid_img and 'image' in valid_img[0]:
+			# 			img_files.append(eachfile)
+			# 	# print "\n final list: ", img_files
+			if img_files:
+				getImgRowHTML(img_files, body_DOM)
 
 
-				# print "\n\n img_files", img_files
+			# 	# print "\n\n img_files", img_files
 
-		if ele_type == "Video":
+		if ele_type.strip() == "Video" and ele_val != u'NONE':
 			'''
 				<video id="[filename without .mp4 here]" class="video-js vis-skin-colors-clix\
 				vjs-big-play-centered" controls="controls" preload="auto" width="480" height="360" data-setup='{}'>
@@ -232,95 +250,107 @@ def parseOnScreenEle(dictionary_obj, body_DOM):
 				href="http://videojs.com/html5-video-support/" \
 				target="_blank">supports HTML5 video</a></p>
 				</video>
+
 			'''
-			sheet_videos_path = 'videos/' + each_sheet.name
-			if os.path.exists(sheet_videos_path):
-				video_filelist = os.listdir(sheet_videos_path)
-				vid_files = []
-				for each_video in video_filelist:
-					try:
-						valid_video = mimetypes.guess_type(sheet_videos_path + '/' +each_video)
-					except Exception as valid_video_err:
-						valid_video = None
-						# print valid_video_err
-					if valid_video and valid_video[0] and 'video' in valid_video[0]:
-						vid_files.append(each_video)
-				if vid_files:
-					getVideoRowHTML(vid_files, body_DOM)
+			vid_files = []
+			vid_files = ele_val.split(',')
+			print vid_files
+
+
+			# sheet_videos_path = 'videos/' + each_sheet.name
+			# if os.path.exists(sheet_videos_path):
+			# 	video_filelist = os.listdir(sheet_videos_path)
+			# 	vid_files = []
+			# 	for each_video in video_filelist:
+			# 		try:
+			# 			valid_video = mimetypes.guess_type(sheet_videos_path + '/' +each_video)
+			# 		except Exception as valid_video_err:
+			# 			valid_video = None
+			# 			# print valid_video_err
+			# 		if valid_video and valid_video[0] and 'video' in valid_video[0]:
+			# 			vid_files.append(each_video)
+			if vid_files:
+				getVideoRowHTML(vid_files, body_DOM)
 		'''
-		if ele_type == "Word cloud":
+		if ele_type.strip() == "Word cloud" and ele_val != u'NONE':
 			pass
 		'''
-		if ele_type == "Audio Track":
+		if ele_type.strip() == "Audio Track" and ele_val != u'NONE':
 			'''
 			<audio width="360" height="360" controls="" class="span_12_of_12">
 				<source src="../Audio/" type="audio/mpeg"/>
 			</audio>
-			section_ele = body_DOM.section
-			section_ele.audio(new_val,escape=False)
 			'''
+
+			audio_ele_data = {
+				'width': '360',
+				'height': '360',
+				'klass':'span_12_of_12',
+			}
+			audio_ele = body_DOM.audio(**audio_ele_data)
+			audio_ele.source(src='../Audio/' + ele_val, type='audio/mpeg')
+
+		if ele_type.strip() == "Subtitles (Audio)" and ele_val != u'NONE':
 			pass
-		if ele_type == "Subtitles (Audio)":
-			pass
-		if ele_type == "Subtitles (Video)":
+		if ele_type.strip() == "Subtitles (Video)" and ele_val != u'NONE':
 			pass
 	# print "\n body_DOM == ", body_DOM
 	return body_DOM
 try:
-	for each_sheet in workbook.sheets():
+	for each_sheet in activities_template_workbook.sheets():
 		# print " || No. of rows found: ", each_sheet.nrows
-		if "Activity" in each_sheet.name:
-			print "\n Processing: ", each_sheet.name,
-			html_content = header_ele = None
-			body_html = HTML('body')
-			for each_row in xrange(each_sheet.nrows):
-				val_list = each_sheet.row_values(each_row)
-				val_list = map(unicode,val_list)
-				# print "\n val list = ", val_list
-				if "Activity Number" in val_list:
-					try:
-						activity_num = val_list[1]
-					except IndexError as name_not_found:
-						print "\n Error !! ", name_not_found
-						pass
+		# if "Activity" in each_sheet.name:
+		print "\n Processing: ", each_sheet.name,
+		html_content = header_ele = None
+		body_html = HTML('body')
+		for each_row in xrange(each_sheet.nrows):
+			val_list = each_sheet.row_values(each_row)
+			val_list = map(unicode,val_list)
+			# print "\n val list = ", val_list
+			if "Activity Number" in val_list:
+				try:
+					activity_num = val_list[1]
+				except IndexError as name_not_found:
+					print "\n Error !! ", name_not_found
+					pass
 
-				if "Activity Name" in val_list:
-					try:
-						activity_name = val_list[2]
-					except IndexError as name_not_found:
-						print "\n Error !! ", name_not_found
-						pass
-				unplatform_row_exists = any('UnPlatform' in each_row for each_row in val_list)
-				# print "\n unplatform_row_exists", unplatform_row_exists
+			if "Activity Name" in val_list:
+				try:
+					activity_name = val_list[2]
+				except IndexError as name_not_found:
+					print "\n Error !! ", name_not_found
+					pass
+			unplatform_row_exists = any('UnPlatform' in each_row for each_row in val_list)
+			# print "\n unplatform_row_exists", unplatform_row_exists
 
-				# if "On Screen / UnPlatform " in val_list:
-				if unplatform_row_exists:
-					dict_obj = fill_dict(each_sheet, each_row)
-					# print "\n final dict = ", dict_obj
-					if dict_obj:
-						if activity_num and activity_name:
-							header_ele = createHTMLHeaderElement(activity_num, activity_name, body_html)
-						main_tag_in_body = body_html.main(klass="group span_10_of_12")
-						html_content = parseOnScreenEle(dict_obj, main_tag_in_body)
-						continue
+			# if "On Screen / UnPlatform " in val_list:
+			if unplatform_row_exists:
+				dict_obj = fill_dict(each_sheet, each_row)
+				# print "\n final dict = ", dict_obj
+				if dict_obj:
+					if activity_num and activity_name:
+						header_ele = createHTMLHeaderElement(activity_num, activity_name, body_html)
+					main_tag_in_body = body_html.main(klass="group span_10_of_12")
+					html_content = parseOnScreenEle(dict_obj, main_tag_in_body)
+					continue
 
-			# print "\n html_content: "
-			# if (html_content):
-			# 	print "Yes"
-			# else:
-			# 	print "No"
-			# print "\n h1_ele: ", html_content
-			# print "\n h2_ele: ", header_ele
-			if body_html:
-				w = open(str(each_sheet.name)+'.html', 'w+')
-				w.write(doc_defination + str(init_html())+str(head_block())+str(body_html)+"</html>")
-				w.close()
-				# print "\n sheet_obj : ", each_sheet.name
-				print "\n Created file: ", each_sheet.name
-				print "*"*80
-		else:
-			print "\n Skipping: ", each_sheet.name
+		# print "\n html_content: "
+		# if (html_content):
+		# 	print "Yes"
+		# else:
+		# 	print "No"
+		# print "\n h1_ele: ", html_content
+		# print "\n h2_ele: ", header_ele
+		if body_html:
+			w = open(str(each_sheet.name)+'.html', 'w+')
+			w.write(doc_defination + str(init_html())+str(head_block())+str(body_html)+"</html>")
+			w.close()
+			# print "\n sheet_obj : ", each_sheet.name
+			print "\n Created file: ", each_sheet.name
 			print "*"*80
+		# else:
+		# 	print "\n Skipping: ", each_sheet.name
+		# 	print "*"*80
 
 except Exception as e:
 	print "\n Error occurred!!!  ", e
